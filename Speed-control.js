cd0557165f5c287c8ddeaecae9e7b09a8f617691
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-14 10:44:38 -0700

    Update Speed-control.js

--- Speed-control.js
+++ Speed-control.js
@@ -26,3 +26,15 @@ To calculate hourly speed you can use: (3600 * delta_distance).
 
 // Solution
 
+const gps = (s, x) => {
+  
+  if (x.length<=1) {
+    return 0;
+  }
+
+  let output = [];
+  for (let i = 0; i < x.length-1; i++) {
+    output.push((x[i+1]-x[i])*3600/s);
+  }
+  return Math.max(...output);
+}
